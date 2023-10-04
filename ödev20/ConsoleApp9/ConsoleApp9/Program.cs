8b58b4145fbd3355e9bb996033f18f690e03accd
Ceren Kaya <cerenkaya1997ck@hotmail.com>
2023-10-04 10:50:00 +0300

    ODEVDENEME

---
+++ ödev20/ConsoleApp9/ConsoleApp9/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] harfler = "TÜRKİYE".ToCharArray();
+
+            // harfleri foreach kullanarak satır satır yazdır:
+            foreach (char c in harfler)
+                Console.WriteLine(c+" ");
+            Console.ReadKey();
+        }
+    }
+}
