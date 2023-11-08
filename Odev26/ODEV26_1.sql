1e46c66a3e5df63f05968c3b5284b2b789eba4c7
Emine <emineclsknn17@gmail.com>
2023-11-09 00:55:12 +0300

    odev26

---
+++ Odev26/ODEV26_1.sql
@@ -0,0 +1,41 @@
+﻿USE master;
+GO
+
+DROP DATABASE IF EXISTS OkulBilgiSistemiDb;
+GO
+
+CREATE DATABASE  OkulBilgiSistemiDb;
+GO
+
+CREATE TABLE Danismanlar
+(
+	Id INT PRIMARY KEY IDENTITY(1, 1),
+	Ad NVARCHAR(50) NOT NULL,
+	SOYAD NVARCHAR(50) NOT NULL
+);
+GO
+
+CREATE TABLE Talebeler
+(
+	Id INT PRIMARY KEY IDENTITY(1, 1),
+	Ad NVARCHAR(50) NOT NULL,
+	SOYAD NVARCHAR(50) NOT NULL,
+	DanismanId INT FOREIGN KEY REFERENCES Danismanlar(Id) NULL
+);
+GO
+
+
+INSERT INTO Danismanlar(Ad,Soyad) VALUES 
+(N'Ali',N'Kara'),(N'Emine',N'Çalışkan'),(N'Mete',N'Ceylan'), (N'Halil','Kahraman');
+INSERT INTO Talebeler(Ad,Soyad,DanismanId) VALUES 
+(N'Ece',N'Gezdir',1),(N'Emre',N'Ay',2),(N'Ada',N'Al',1), (N'Eylül',N'Gece',1),(N'Oya',N'Say',1),(N'Ela',N'Kara',3),(N'Alper',N'Yıldız',2), (N'Eylül',N'Gece',4),(N'Seda',N'Güler',2), (N'Seher',N'Akbaş',3);
+ 
+--SELECT * FROM  Talebeler INNER JOIN Danismanlar ON Danismanlar.Id=Talebeler.DanismanId
+
+SELECT
+    Talebeler.Ad      AS TalebeAd,
+    Talebeler.Soyad   AS TalebeSoyad,
+    Danismanlar.Ad    AS DanismanAd,
+    Danismanlar.Soyad AS DanismanSoyad
+FROM Talebeler
+INNER JOIN Danismanlar ON Talebeler.DanismanId = Danismanlar.Id;
---
+++ Odev26/Odev26_2.sql
@@ -0,0 +1,22 @@
+﻿
+--2) Order Details tablosu ile Products tablosunu inner join yaparak dicount'u sıfırdan farklı olanların ProductName ve UnitPrice kolonlarını getiriniz.
+SELECT  Products.ProductName[Product Name], Products.UnitPrice [Products Unit Price],[Order Details].UnitPrice [Order details unit price] FROM Products INNER JOIN [Order Details] ON [Order Details].ProductID=Products.ProductID Where Discount !=0
+
+--3)Products tablosu ile Suppliers tablosunu inner join yaparak Supplier'ın şehri (city) New Orleans olanların, city, companyname ve productname'lerini, productname'e göre azalan sırada sıralayarak getiriniz.
+SELECT ProductName,City,CompanyName FROM Products INNER JOIN Suppliers ON Products.SupplierID=Suppliers.SupplierID WHERE City=N'New Orleans' ORDER BY ProductName
+
+--4)TerritoryDescripttion'ı Boston olanın RegionDescription'ının bulunuz. (Inner join kullanarak)
+
+SELECT Region.RegionDescription FROM Region INNER JOIN Territories ON Territories.RegionID=Region.RegionID WHERE Territories.TerritoryDescription = 'Boston';
+
+--5)CategoryName'i Seefood VEYA Cheeses olan VEYA  description'ı cheeses olanları ve discontinued değeri 1 olanları ProductName,CategoryName ve Discontinued değerlerini inner join ile getiriniz.
+--YAPAMADIM
+SELECT Products.ProductName, Categories.CategoryName, Products.Discontinued
+FROM Products  
+INNER JOIN Categories ON Products.CategoryID = Categories.CategoryID
+WHERE ( CategoryName = 'Seafood' OR CategoryName = 'Cheeses' ) OR (Categories.Description='cheeses'  AND Products.Discontinued = 1);
+
+--6
+SELECT Birinci.FirstName AS EmployeeFirstName, Birinci.LastName AS EmployeeLastName, İkinci.FirstName AS ManagerFirstName, İkinci.LastName AS ManagerLastName
+FROM Employees Birinci
+INNER JOIN Employees İkinci ON Birinci.ReportsTo = İkinci.EmployeeID;
